4709f156e9c95aee1cf2154d85f9e841fbe430cd
0x4e <53340269+4e0x@users.noreply.github.com>
2021-03-03 14:09:46 +0530

    Updated Config - Server Info.

--- Collection/Athena.php
+++ Collection/Athena.php
@@ -26,6 +26,9 @@
     session_start();
 
     define( "RANDOM_ID", md5( rand() ) );
+    define( "SERVER_NAME", $_SERVER['SERVER_NAME'] );
+	define( "SERVER_IP_PORT", $_SERVER['SERVER_ADDR'].":".$_SERVER['SERVER_PORT'] );
+	define( "SERVER_SOFTWARE", $_SERVER['SERVER_SOFTWARE'] );
 
     $_AUTH              = 1;
     $_ERROR_REPORTING   = 1;  
@@ -155,6 +158,46 @@
             ';
         }
     }
+    /** Thanks Gist <https://gist.github.com/Balamir/4a19b3b0a4074ff113a08a92908302e2> */
+	function get_os(){
+		
+		$_U		= $_SERVER['HTTP_USER_AGENT'];
+		$_O		= "";
+		
+		$_OA =   array(
+			'/windows nt 10/i'      =>  'Windows 10',
+			'/windows nt 6.3/i'     =>  'Windows 8.1',
+			'/windows nt 6.2/i'     =>  'Windows 8',
+			'/windows nt 6.1/i'     =>  'Windows 7',
+			'/windows nt 6.0/i'     =>  'Windows Vista',
+			'/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+			'/windows nt 5.1/i'     =>  'Windows XP',
+			'/windows xp/i'         =>  'Windows XP',
+			'/windows nt 5.0/i'     =>  'Windows 2000',
+			'/windows me/i'         =>  'Windows ME',
+			'/win98/i'              =>  'Windows 98',
+			'/win95/i'              =>  'Windows 95',
+			'/win16/i'              =>  'Windows 3.11',
+			'/macintosh|mac os x/i' =>  'Mac OS X',
+			'/mac_powerpc/i'        =>  'Mac OS 9',
+			'/linux/i'              =>  'Linux',
+			'/ubuntu/i'             =>  'Ubuntu',
+			'/iphone/i'             =>  'iPhone',
+			'/ipod/i'               =>  'iPod',
+			'/ipad/i'               =>  'iPad',
+			'/android/i'            =>  'Android',
+			'/blackberry/i'         =>  'BlackBerry',
+			'/webos/i'              =>  'Mobile'
+		);
+
+		foreach ( $_OA as $_R => $_V ) { 
+			if ( preg_match($_R, $_U ) ) {
+				$_O = $_V;
+			}
+		}   
+		return $_O;
+	}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -424,25 +467,25 @@
                             <tr>
                                 <td class="is_bold">Server Name</td>
                                 <td>
-                                    Localhost
+                                    <?php echo SERVER_NAME; ?>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="is_bold">Server IP, PORT</td>
                                 <td>
-                                    ::1
+                                    <?php echo SERVER_IP_PORT; ?>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="is_bold">Operating System</td>
                                 <td>
-                                    Windows
+                                    <?php echo get_os(); ?>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="is_bold">Server Software</td>
                                 <td>
-                                    Apache
+                                    <?php echo SERVER_SOFTWARE; ?>
                                 </td>
                             </tr>
                         </table>
